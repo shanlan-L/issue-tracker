d1237b7f2de9fe9cbc574660775dd131c874b961
lichengrui <lichengrui@exiao.tech>
2024-01-29 23:41:34 +0800

    ✨ Cusomize RadixUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Theme, ThemePanel } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,9 +23,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <NavBar />
-        <main>{children}</main>
+      <body className={inter.variable}>
+        <Theme accentColor="cyan">
+          <NavBar />
+          <main className="p-5">{children}</main>
+          <ThemePanel />
+        </Theme>
       </body>
     </html>
   );
