ae3171b57120730a69501226fb03edab710a192f
lichengrui <lichengrui@exiao.tech>
2024-01-30 22:24:03 +0800

    ✨ Complete add issues and issues table

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 
 const inter = Inter({
@@ -27,7 +27,6 @@ export default function RootLayout({
         <Theme accentColor="cyan">
           <NavBar />
           <main className="p-5">{children}</main>
-          <ThemePanel />
         </Theme>
       </body>
     </html>
