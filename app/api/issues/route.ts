3d10bb4c1f96c70b2ae8f291062e9a5845c85189
lichengrui <lichengrui@exiao.tech>
2024-02-01 00:12:33 +0800

    ✨ add update api

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,10 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { createIssueSchema } from "../../validationSchemas";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const issueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required."),
+});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
-  const validation = createIssueSchema.safeParse(body);
+  const validation = issueSchema.safeParse(body);
 
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), { status: 400 });
